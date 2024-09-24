77ffc616a57890f7f2eed2b389a8bea115cedc73
alberto <>
2024-09-24 12:55:09 +0200

    Clase Estado

---
+++ tests.py
@@ -0,0 +1,29 @@
+import json
+
+with open('/home/calberto/Documents/Uni/5toCuatri/SistemasInteligentes/Practicas/P1/pr1_SSII/problems/small/avenida_de_espana_250_0.json', 'r') as file:
+    new_dictionary = json.load(file)
+
+print (new_dictionary)
+print("------------------------")
+#El diccionario del json, en el apartado intersections tiene otro diccionario
+print (new_dictionary["intersections"])
+print("------------------------")
+print (new_dictionary["segments"])
+
+
+#intersections = {"identifier": ,"longitude": ,"latitude": }
+
+#Hacemos el diccionario intersections que tiene el diccionario dentro de new_dictionary[intersections]
+intersections = new_dictionary["intersections"]
+segments = new_dictionary["segments"]
+
+class Estado:
+    def __init__(self,id,latitud,longitud):
+        self.id = id
+        self.latitud = latitud
+        self.longitud = longitud
+
+estado = Estado(3,4,5)
+print(estado.id)
+print(estado.latitud)
+print(estado.longitud)
