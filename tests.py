dc82695b76d7276fef19834147ec4d3e91592883
alberto <alberto.perez25@alu.uclm.es>
2024-10-03 20:24:46 +0200

    Implementacion inicial Clases Basicas

--- tests.py
+++ tests.py
@@ -4,102 +4,103 @@ import json
 with open('/home/calberto/Documents/Uni/5toCuatri/SistemasInteligentes/Practicas/P1/pr1_SSII/problems/small/avenida_de_espana_250_0.json', 'r') as file:
     new_dictionary = json.load(file)
 
-print (new_dictionary)
+#print (new_dictionary)
 print("------------------------")
 #El diccionario del json, en el apartado intersections tiene otro diccionario
-print (new_dictionary["intersections"])
+#print (new_dictionary["intersections"])
 print("------------------------")
-print (new_dictionary["segments"])
+#print (new_dictionary["segments"])
 
 
 #intersections = {"identifier": ,"longitude": ,"latitude": }
 
 #Hacemos el diccionario intersections que tiene el diccionario dentro de new_dictionary[intersections]
-intersections = new_dictionary["intersections"]
-segments = new_dictionary["segments"]
+#intersections = new_dictionary["intersections"]
+#segments = new_dictionary["segments"]
 
-#Hacemos un objeto
-class Estado: #En que interseccion estas son sus segmentos
-    def __init__(self, id, interseccion):
-        self.id = id
-        self.interseccion = interseccion
+# Load JSON data
+with open('/home/calberto/Documents/Uni/5toCuatri/SistemasInteligentes/Practicas/P1/pr1_SSII/problems/small/avenida_de_espana_250_0.json', 'r') as file:
+    data = json.load(file)
+
+class Intersection:
+    def __init__(self, id, latitude, longitude):
+        self.identifier = id
+        self.latitude = latitude
+        self.longitude = longitude
 
     def __str__(self):
-        return f"State(id={self.id}, intersection_id={self.interseccion})"
+        return f"Intersection(id={self.identifier}, latitude={self.latitude}, longitude={self.longitude})"
 
-    def __repr__(self):
-        return self.__str__()
+# Convert intersections
+intersections = set()
+for intersection in data['intersections']:
+    identifier = intersection['identifier']
+    intersections.add(Intersection(identifier, intersection['latitude'], intersection['longitude']))
 
-    def get_interseccion(self):
-        return self.interseccion
+class Segment:
+    def __init__(self, origin, destination, distance, speed):
+        self.origin = origin
+        self.destination = destination
+        self.distance = distance
+        self.speed = speed
 
-    def update_interseccion(self, new_interseccion_id):
-        self.interseccion = new_interseccion_id
+segments = set()
+for seg in data['segments']:
+    segments.add(Segment(seg['origin'], seg['destination'], seg['distance'], seg['speed']))
+
+
+
+
+intersection_dic = {i.identifier: i for i in intersections}
+segment_dic = {(s.origin, s.destination): s for s in segments}
+
+print(intersection_dic[1530850331])
+print(intersection_dic[1530850331].latitude)
+
+
+######################################################################
+
+#Hacemos un objeto
+class Estado: #En que interseccion estas con sus segmentos
+    def __init__(self, intersection):
+        self.identifier = intersection.identifier
+        self.latitude = intersection.latitude
+        self.longitude = intersection.longitude
+    
+    def getSegments(self):
+        return Nodo.getSegmentsOf(self.identifier)
+    
+    def setCurrent(self, intersection):
+        self.__init__(intersection)
+   
 class Accion: #Irnos por un segmento (calle) u otra. Al elegir un segmento
     # actualizar Estado con el del campo "final" del segmento escogido
 
-    def __init__(self, interseccion, calleElegida):
-        self.Nodo = interseccion
-        self.calleElegida = calleElegida
-
-    def __str__(self):
-        return f"At the intersection {self.interseccion}, choose {self.calleElegida}"
-
-    def __repr__(self):
-        return f"Action(intersection='{self.interseccion}', chosen_street='{self.calleElegida}')"
-    def irCalle(self):
-        return self.interseccion.get_neighbors()[0]
+    def __init__(self, chosenSegment, intersection = 0):
+        self.intersection = intersection
+        self.chosenSegment = chosenSegment
+    
+    def getchosenIntersection(self):
+        return self.chosenSegment.destination
     
 class Nodo: #representa (creo que todas) las intersecciones y sus segmentos asociados. 
     #Los segmentos se relacionan entre si y con las intersecciones 
     # mediante origin y destination, que contienen ids de intersecciones
-    def __init__(self, id, latitude, longitude,speed, index = 0):
-        self.id = id 
-        self.latitude = latitude
-        self.longitude = longitude
-        self.speed = speed
-        self.neighbors = []
-        self.index = index
+    def __init__(self, intersections, segments):
+        self.intersections = intersections
+        self.segments = segments
 
-    def __str__(self):
-        return f"Node(id={self.id}, latitude={self.latitude}, longitude={self.longitude})"
-
-    def __repr__(self):
-        return self.__str__()
-
-    def add_neighbor(self, neighbor):
-        self.neighbors.append(neighbor)
-
-    def remove_neighbor(self, neighbor):
-        if neighbor in self.neighbors:
-            self.neighbors.remove(neighbor)
-            return True
-        return False
+    def getSegmentsOf(self,id):
+        return self.segments[id]
     
-    def next_neighbor(self):
-        if self.index < len(self.neighbors):
-            self.index=self.index+1
-            return True
-        return False
-    
-    def get_neighbors(self):
-        return self.neighbors
-    
-    def get_currentNeighbor(self):
-        return self.neighbors[self.index]
-
-    def get_coordinates(self):
-        return (self.latitude, self.longitude)
+    def getIntersection(self, id):
+        return self.intersections[id]
 
 class Problema:
-    def __init__(self,id,latitud,longitud):
-        self.id = id
-        self.latitud = latitud
-        self.longitud = longitud
-
-
-#Inicializamos el objeto
-estado = Estado(3,4,5)
-print(estado.id)
-print(estado.latitud)
-print(estado.longitud)
+    def __init__(self,intersection,solutionId):
+        self.intersection = intersection
+        self.solutionId = solutionId
+    
+    def isSolution(self):
+        return self.solutionId == self.intersection.identifier
+    
